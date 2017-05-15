5e50ce6e081d6b17d78eb22d5d7d920e5999b3a3
kubasmolen <jsmolen012@gmail.com>
2017-05-16 00:08:10 +0200

    Daily scrum, przykadowe menu
    Dorzucam szkic menu w unity, może nam się przyda

---
+++ Pliki/Kuba Smoleń/Unity_menu/Assets/LevelHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelHandler : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+
+	
+	public void ChangeScene (string name) {
+		Debug.Log ("Change scene requested");
+		Application.LoadLevel(name);
+	}
+	
+	public void QuiyRequest () {
+		Debug.Log ("Quit game requested");
+		Application.Quit();
+	}
+}
