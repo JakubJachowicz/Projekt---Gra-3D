068d968dbccdbcf3f943f0158bf60d7702bfa564
kubasmolen <jsmolen012@gmail.com>
2017-05-18 22:43:41 +0200

    W menu fonty, ekran startowy

---
+++ Pliki/Kuba Smoleń/Unity_menu/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelHandler : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+	public void ChangeScene (string name) {
+		Debug.Log ("Change scene requested to " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuiyRequest () {
+		Debug.Log ("Quit game requested");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel () {
+		Debug.Log ("Load next scene requested");
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
