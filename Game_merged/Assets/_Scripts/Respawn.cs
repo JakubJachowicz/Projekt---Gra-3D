365b5d85ece5cece184284c363faadd3e60d8681
JakubJachowicz <kuba713jach@gmail.com>
2017-05-31 22:52:06 +0200

    Update commitu #12

---
+++ Game_merged/Assets/_Scripts/Respawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	public GameObject sphereprefab;
+	public Camera cameraprefab;
+	public Light lightprefab;
+	/*
+	public GameObject coinprefab;
+	public int coinTotal = 1000;
+	*/
+	// Use this for initialization
+	void Start () {
+		/*int i;
+		Vector3 * coinPosition = new Vector3 [coinTotal];
+		GameObject * coin = new GameObject [coinTotal];
+		for (i=0; i<coinTotal;i++)
+		{
+			coin[i] = GameObject.Instantiate(coinprefab);
+		}
+		for (i=0; i<coinTotal;i++)
+		{
+			coinPosition[i].x = Random.Range(-2, 3);
+			coinPosition[i].y = Random.Range(1, 3);
+			coinPosition[i].z = Random.Range(10, 1000);
+			coin[i].transform.position = coinPosition[i];
+		}
+		*/
+		GameObject sphere = GameObject.Instantiate(sphereprefab);
+		sphere.name = "Sphere";
+		sphere.transform.position = transform.position;
+		
+		Camera  camera = GameObject.Instantiate(cameraprefab);
+		CameraBehindSphere cameraController = camera.GetComponent<CameraBehindSphere>();
+		cameraController.sphere = sphere.transform;
+		
+		Light mylight = GameObject.Instantiate(lightprefab);
+		LightBehindBall lightController = mylight.GetComponent<LightBehindBall>();
+		lightController.sphere = sphere.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
