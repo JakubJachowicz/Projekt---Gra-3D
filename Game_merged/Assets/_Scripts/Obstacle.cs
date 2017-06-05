089ca9cd4815cf22f64d5f4b0592a76fce5427a9
JakubJachowicz <kuba713jach@gmail.com>
2017-06-05 23:35:45 +0200

    poprawka v2

---
+++ Game_merged/Assets/_Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+	
+	private MovementOptions gameControler;
+	
+	void OnTriggerEnter(Collider collision) {
+
+		if (collision.gameObject.name != "Sphere")
+			return;
+		Application.LoadLevel("Game Over");
+		}
+}
