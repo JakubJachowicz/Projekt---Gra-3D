0522610faf707fe0f69c9f7a55c6833dd4a74b30
staszek123991 <staszek123991@gmail.com>
2017-06-13 07:53:49 +0100

    ostatnia zmianka

---
+++ Game_merged/Assets/_Scripts/WinTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+
+    private LevelHandler levelHandler;
+    public GameObject sphere;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelHandler = GameObject.FindObjectOfType<LevelHandler>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MovementOptions script = sphere.GetComponent<MovementOptions>();
+        int highScore = script.score;
+        PlayerPrefsManager.SetHighScore(highScore);
+        if (other.name == "Sphere")
+            levelHandler.LoadLevel("Win");
+    }
+}
